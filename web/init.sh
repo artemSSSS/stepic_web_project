103aef2601bc9db23c7a429a9533068ff5bdd3ce
artemSSSS <artyom148Dik@mail.ru>
2020-01-08 11:43:29 +0400

    c

--- web/init.sh
+++ web/init.sh
@@ -1,3 +1,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
+
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+
+sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/nginx restart
